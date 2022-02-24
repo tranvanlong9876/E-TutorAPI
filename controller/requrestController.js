67497b1a808d1612b875dfe7180bcb04f954e251
devphu <phankieuphu@gmail.com>
2022-02-25 01:53:23 +0700

    update request

--- controller/requrestController.js
+++ controller/requrestController.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize/types');
 const db = require('../models')
 const Request = db.request;
 
@@ -5,9 +6,9 @@ const Request = db.request;
 const addRequest = async (req,res) =>{
     try {
         let items = {
+            author: req.body.author,
             student: req.body.student,
             course: req.body.course,
-            status:req.body.status
         }
         await Request.create(items);
         return res.status(200).send({Message: 'Course added successfully'});
@@ -19,11 +20,57 @@ const addRequest = async (req,res) =>{
 const getRequrestById = async (req,res) =>{
     
     try{
-        const id = req.params.id;
-        const request = await Request.findAll({author:id});
+        const id = req.params.author;
+        const request = await Request.findAll(
+                {
+                    where: {
+                        author:id,
+                        status:"processing"
+                    }
+                }
+
+            );
         return res.status(200).send( request)
     }catch(err){
         return res.status(400).send({mesage: err.message});
+    }
+
+}
+
+const cancelRequest = async (req,res) =>{
+
+    try{
+        const id = req.params.id;
+        const reposne = await Request.destroy(
+            {       
+                where:{
+                    id:id
+                }
+            }
+        );
+        return res.status(200).send({
+            success:"true",
+            mesage: reposne
+        })
+    }catch(err){
+        return res.status(400).send({
+            success:false,
+            mesage: err.message});
+    }
+
+}
+
+const appeptRequest = async (req,res) =>{
+
+    try{
+        const id = req.params.id;
+        const request = await Request.findById(id);
+        const reponse = request.udpate({
+            status:"true"
+        })
+        return res.status(200).send({success:"true", message:reponse});
+    }catch(err){
+        return res.status(400).send({success:"false", message:err.message});
     }
 
 }
@@ -32,5 +79,7 @@ const getRequrestById = async (req,res) =>{
 
 module.exports = {
     addRequest,
-    getRequrestById
+    getRequrestById,
+    cancelRequest,
+    appeptRequest
 }
--- router/request.router.js
+++ router/request.router.js
@@ -8,7 +8,9 @@ router.post('/createRequest',requrestController.addRequest);
 
 router.get('/getRequest/:id',requrestController.getRequrestById);
 
+router.post('/appeptRequest/:id',requrestController.appeptRequest);
 
+router.post('cancelRequest/:author',requrestController.cancelRequest);
 
 
 module.exports = router;
